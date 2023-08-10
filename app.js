ab19e3eaa749f21ad473da3a3c07a566fae2c0ce
Rob Schuster <robert.is@icloud.com>
2023-08-09 22:09:54 -0400

    Begin to calculate results

--- app.js
+++ app.js
@@ -11,3 +11,20 @@ document.getElementById('loan-form').addEventListener('submit', function(e){
 
   e.preventDefault();
 });
+
+// calculate results
+function calculateResults() {
+  // UI vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  //compute monthly payment
+}

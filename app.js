59195a60fb840e3963c38a3ef67b987becaa7110
Rob Schuster <robert.is@icloud.com>
2023-08-14 19:02:29 -0400

    complete js, complete html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*jslint esversion:6*/
 
-// Listen for submit
+// Listen for submit event
 document.getElementById('loan-form').addEventListener('submit', function(e){
   //hide results
   document.getElementById('results').style.display = 'none';
@@ -12,7 +12,7 @@ document.getElementById('loan-form').addEventListener('submit', function(e){
   e.preventDefault();
 });
 
-// calculate results
+// results calculations
 function calculateResults() {
   // UI vars
   const amount = document.getElementById('amount');
@@ -26,7 +26,43 @@ function calculateResults() {
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
-  //compute monthly payment
+  // monthly payment calculations
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest)/(x-1);
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    // show calculation results
+    document.getElementById('results').style.display = 'block';
+    // hide loader
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Please check your numbers');
+  }
+}
+
+// Error show
+function showError(error) {
+  // show results
+  document.getElementById('results').style.display = 'none';
+  // hide loader
+  document.getElementById('loading').style.display = 'none';
+  // create a div
+  const errorDiv = document.createElement('div');
+  // get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+  // add class
+  errorDiv.className = 'alert alert-danger';
+  // create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  // Insert error above Heading
+  card.insertBefore(errorDiv, heading);
+  // clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
 }
